a5f7a08a687c3de0bd1063403d1cb7a15a283f99
Nikita_Pl <bruuix@gmail.com>
2026-01-23 16:11:49 +0700

    Added modal window for choosing players to command

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,10 @@
-// import { supabase } from "../../services/supabase/supabase";
-// import { useEffect, useState } from "react";
 import { MatchesBlock } from "../../components/matchSlider/MathcesBlock";
 import { PlayerBlock } from "../../components/playerCard/PlayerBlock";
 
 function Home() {
-  // const [users, setUsers] = useState([]);
-
-  // useEffect(() => {
-  //   async function getUsers() {
-  //     const { data } = await supabase.from("users").select();
-  //     setUsers(data);
-  //   }
-  //   getUsers();
-  // }, []);
-
   return (
     <>
-      <MatchesBlock/>
+      <MatchesBlock />
       <PlayerBlock />
     </>
   );
--- src/Pages/MatchCreation/MatchCreation.jsx
+++ src/Pages/MatchCreation/MatchCreation.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import plus from "../../assets/images/plus.svg";
+import photo from "../../assets/images/playerCard.jpg";
 import styles from "./MatchCreation.module.css";
 
-function MatchCreation() {
+function MatchCreation({ users }) {
+  const [open, setOpen] = useState(false);
+  console.log(open);
+  console.log(users);
+
+  function toggleModal(event) {
+    event.preventDefault();
+    console.log("click");
+    open ? setOpen(false) : setOpen(true);
+  }
+
   return (
     <>
       <h1 className={styles.title}>Match creation</h1>
@@ -13,23 +25,67 @@ function MatchCreation() {
           </div>
           <div className={styles.inputBlock}>
             <label htmlFor="date">Match date</label>
-            <input type="date" name="date" value="2026-01-01"/>
+            <input type="date" name="date" />
           </div>
-
+          <div
+            className={`${styles.users__list} ${open ? `${styles.open}` : ""}`}
+          >
+            <div
+              style={{
+                textAlign: "center",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <h1>Team one</h1>
+              <span>0 from 5</span>
+              {users.map((user) => {
+                return (
+                  <div className={styles.wrapper} key={user.id}>
+                    <input
+                      className={styles.checkbox}
+                      type="checkbox"
+                      name={user.id}
+                    />
+                    <img
+                      className={styles.player__img}
+                      src={photo}
+                      alt={user.name}
+                    />
+                    <p className={styles.player__name}>
+                      {user.name} {user.surname}
+                    </p>
+                  </div>
+                );
+              })}
+              <button
+                type="button"
+                className={styles.button}
+                onClick={toggleModal}
+              >
+                Add
+              </button>
+            </div>
+          </div>
           <div className={styles.inputBlock}>
             <label htmlFor="team-one">Team one</label>
-            <button className={styles.button}>
+            <button
+              className={styles.button}
+              onClick={(event) => {
+                toggleModal(event);
+              }}
+            >
               <img src={plus} alt="Add" />
             </button>
           </div>
           <div className={styles.inputBlock}>
-            <label htmlFor="team-one">Team one</label>
+            <label htmlFor="team-two">Team two</label>
             <button className={styles.button}>
               <img src={plus} alt="Add" />
             </button>
           </div>
 
-          <input type="submit" value="Create" />
+          <input type="submit" value="Create" disabled />
         </div>
       </form>
     </>
