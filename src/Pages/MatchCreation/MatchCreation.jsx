e30e57aa0148d328240d18d233ef2e0d02c81dbb
Nikita_Pl <bruuix@gmail.com>
2026-01-30 14:17:47 +0700

    - Created new component for matchform - Conntected form sending via rpc to DB

--- src/Pages/MatchCreation/MatchCreation.jsx
+++ src/Pages/MatchCreation/MatchCreation.jsx
@@ -1,16 +1,9 @@
-// TO DO 
-// 1. Убрать форму в отдельеный компонент 
-// 2. заменить styles на className в JSX
-
-
 import { useState } from "react";
-import plus from "../../assets/images/plus.svg";
-import photo from "../../assets/images/playerCard.jpg";
+import { MatchForm } from "../../components/matchForm/matchForm";
+import { supabase } from "../../services/supabase/supabase";
 import styles from "./MatchCreation.module.css";
 
 function MatchCreation({ users }) {
-  const [openTeamOne, setOpenTeamOne] = useState(false);
-  const [openTeamTwo, setOpenTeamTwo] = useState(false);
   const [teamOne, setTeamOne] = useState([]);
   const [teamTwo, setTeamTwo] = useState([]);
 
@@ -21,15 +14,6 @@ function MatchCreation({ users }) {
   const availableTeamTwo = users.filter((user) => {
     return !teamOne.some((player) => player.id === user.id);
   });
-
-  function toggleModal(event, team) {
-    event.preventDefault();
-    if (team === "teamOne") {
-      setOpenTeamOne((prev) => !prev);
-    } else if (team === "teamTwo") {
-      setOpenTeamTwo((prev) => !prev);
-    }
-  }
 
   function addPlayersToTeam(event, user, team) {
     if (team === "teamOne")
@@ -52,210 +36,38 @@ function MatchCreation({ users }) {
     }
   }
 
-  function submitForm(formData) {
-    const matchName = formData.get("matchName");
+  async function submitForm(formData) {
+    // const matchName = formData.get("matchName");
     const matchDate = formData.get("matchDate");
 
-    const date = new Date(matchDate).toString();
+    const date = new Date(matchDate).toISOString();
+    const teamOneIds = teamOne.map((player) => player.id);
+    const teamTwoIds = teamTwo.map((player) => player.id);
 
-    const match = {
-      matchName: matchName,
-      matchDate: Date.parse(date),
-      teamOne: teamOne,
-      teamTwo: teamTwo,
-    };
+    const { error } = await supabase.rpc("create_match", {
+      team_one_name: "teamOne",
+      team_two_name: "teamTwo",
+      team_one_players: teamOneIds,
+      team_two_players: teamTwoIds,
+      match_start_at: date,
+    });
 
-    console.log(match);
+    if (error) {
+      console.log(error.message);
+    }
   }
 
   return (
     <>
       <h1 className={styles.title}>Match creation</h1>
-      <form action={submitForm}>
-        <div className={styles.wrapper}>
-          <div className={styles.inputBlock}>
-            <label>Match name</label>
-            <input type="text" name="matchName" placeholder="Match name" />
-          </div>
-          <div className={styles.inputBlock}>
-            <label>Match date</label>
-            <input type="date" name="matchDate" />
-          </div>
-          <div
-            className={`${styles.users__list} ${openTeamOne ? `${styles.open}` : ""}`}
-          >
-            <h1>Team one</h1>
-            <span>{teamOne.length} from 5</span>
-            <div className={styles.userScroll}>
-              {availableTeamOne.map((user) => {
-                return (
-                  <div className={styles.wrapper} key={user.id}>
-                    <input
-                      className={styles.checkbox}
-                      type="checkbox"
-                      name="player"
-                      checked={teamOne.some((player) => player.id === user.id)}
-                      onChange={(event) =>
-                        addPlayersToTeam(event, user, "teamOne")
-                      }
-                      disabled={
-                        teamOne.length === 5 &&
-                        !teamOne.some((player) => player.id === user.id)
-                      }
-                    />
-                    <img
-                      className={styles.player__img}
-                      src={photo}
-                      alt={user.name}
-                    />
-                    <p className={styles.player__name}>
-                      {user.name} {user.surname}
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-            <button
-              type="button"
-              className={styles.button}
-              onClick={(event) => {
-                toggleModal(event, "teamOne");
-              }}
-            >
-              Close
-            </button>
-          </div>
-          <div className={styles.inputBlock}>
-            <label htmlFor="team-one">Team one</label>
-            <div
-              style={{
-                marginBottom: "10px",
-                display: "flex",
-                flexDirection: "row",
-                gap: "22px",
-                overflow: "scroll",
-              }}
-            >
-              {teamOne.map((player) => {
-                return (
-                  <div key={player.id}>
-                    <img
-                      className={styles.player__img}
-                      src={photo}
-                      alt={player.name}
-                    />
-                    <p
-                      style={{
-                        fontSize: "18px",
-                        fontWeight: "700",
-                        margin: "0px",
-                        textAlign: "center",
-                      }}
-                    >
-                      {player.name.slice(0, 1)}.{player.surname.slice(0, 1)}.
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-            <button
-              className={styles.button}
-              onClick={(event) => {
-                toggleModal(event, "teamOne");
-              }}
-            >
-              {!teamOne.length ? <img src={plus} alt="Add" /> : "Edit"}
-            </button>
-          </div>
-          <div
-            className={`${styles.users__list} ${openTeamTwo ? `${styles.open}` : ""}`}
-          >
-            <h1>Team two</h1>
-            <span>{teamTwo.length} from 5</span>
-            <div className={styles.userScroll}>
-              {availableTeamTwo.map((user) => {
-                return (
-                  <div className={styles.wrapper} key={user.id}>
-                    <input
-                      className={styles.checkbox}
-                      type="checkbox"
-                      name="player"
-                      checked={teamTwo.some((player) => player.id === user.id)}
-                      onChange={(event) =>
-                        addPlayersToTeam(event, user, "teamTwo")
-                      }
-                      disabled={
-                        teamTwo.length === 5 &&
-                        !teamTwo.some((player) => player.id === user.id)
-                      }
-                    />
-                    <img
-                      className={styles.player__img}
-                      src={photo}
-                      alt={user.name}
-                    />
-                    <p className={styles.player__name}>
-                      {user.name} {user.surname}
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-            <button
-              type="button"
-              className={styles.button}
-              onClick={(event) => {
-                toggleModal(event, "teamTwo");
-              }}
-            >
-              Close
-            </button>
-          </div>
-          <div className={styles.inputBlock}>
-            <label htmlFor="team-two">Team two</label>
-            <div
-              style={{
-                marginBottom: "10px",
-                display: "flex",
-                flexDirection: "row",
-                gap: "22px",
-                overflow: "scroll",
-              }}
-            >
-              {teamTwo.map((player) => {
-                return (
-                  <div key={player.id}>
-                    <img
-                      className={styles.player__img}
-                      src={photo}
-                      alt={player.name}
-                    />
-                    <p
-                      style={{
-                        fontSize: "18px",
-                        fontWeight: "700",
-                        margin: "0px",
-                        textAlign: "center",
-                      }}
-                    >
-                      {player.name.slice(0, 1)}.{player.surname.slice(0, 1)}.
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-            <button
-              className={styles.button}
-              onClick={(event) => {
-                toggleModal(event, "teamTwo");
-              }}
-            >
-              {!teamTwo.length ? <img src={plus} alt="Add" /> : "Edit"}
-            </button>
-          </div>
-          <input type="submit" value="Create" />
-        </div>
-      </form>
+      <MatchForm
+        teamOne={teamOne}
+        teamTwo={teamTwo}
+        availableTeamOne={availableTeamOne}
+        availableTeamTwo={availableTeamTwo}
+        addPlayersToTeam={addPlayersToTeam}
+        submitForm={submitForm}
+      />
     </>
   );
 }
---
+++ src/components/matchForm/MatchForm.jsx
@@ -0,0 +1,222 @@
+import { useState } from "react";
+import plus from "../../assets/images/plus.svg";
+import photo from "../../assets/images/playerCard.jpg";
+import styles from "./matchForm.module.css";
+
+function MatchForm({
+  teamOne,
+  teamTwo,
+  availableTeamOne,
+  availableTeamTwo,
+  addPlayersToTeam,
+  submitForm,
+}) {
+  const [openTeamOne, setOpenTeamOne] = useState(false);
+  const [openTeamTwo, setOpenTeamTwo] = useState(false);
+
+  function toggleModal(event, team) {
+    event.preventDefault();
+    if (team === "teamOne") {
+      setOpenTeamOne((prev) => !prev);
+    } else if (team === "teamTwo") {
+      setOpenTeamTwo((prev) => !prev);
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target)
+        await submitForm(formData);
+      }}>
+        <div className={styles.wrapper}>
+          <div className={styles.inputBlock}>
+            <label>Match name</label>
+            <input type="text" name="matchName" placeholder="Match name" />
+          </div>
+          <div className={styles.inputBlock}>
+            <label>Match date</label>
+            <input type="date" name="matchDate" />
+          </div>
+          <div
+            className={`${styles.users__list} ${openTeamOne ? `${styles.open}` : ""}`}
+          >
+            <h1>Team one</h1>
+            <span>{teamOne.length} from 5</span>
+            <div className={styles.userScroll}>
+              {availableTeamOne.map((user) => {
+                return (
+                  <div className={styles.wrapper} key={user.id}>
+                    <input
+                      className={styles.checkbox}
+                      type="checkbox"
+                      name="player"
+                      checked={teamOne.some((player) => player.id === user.id)}
+                      onChange={(event) =>
+                        addPlayersToTeam(event, user, "teamOne")
+                      }
+                      disabled={
+                        teamOne.length === 5 &&
+                        !teamOne.some((player) => player.id === user.id)
+                      }
+                    />
+                    <img
+                      className={styles.player__img}
+                      src={photo}
+                      alt={user.name}
+                    />
+                    <p className={styles.player__name}>
+                      {user.name} {user.surname}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+            <button
+              type="button"
+              className={styles.button}
+              onClick={(event) => {
+                toggleModal(event, "teamOne");
+              }}
+            >
+              Close
+            </button>
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor="team-one">Team one</label>
+            <div
+              style={{
+                marginBottom: "10px",
+                display: "flex",
+                flexDirection: "row",
+                gap: "22px",
+                overflow: "scroll",
+              }}
+            >
+              {teamOne.map((player) => {
+                return (
+                  <div key={player.id}>
+                    <img
+                      className={styles.player__img}
+                      src={photo}
+                      alt={player.name}
+                    />
+                    <p
+                      style={{
+                        fontSize: "18px",
+                        fontWeight: "700",
+                        margin: "0px",
+                        textAlign: "center",
+                      }}
+                    >
+                      {player.name.slice(0, 1)}.{player.surname.slice(0, 1)}.
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+            <button
+              className={styles.button}
+              onClick={(event) => {
+                toggleModal(event, "teamOne");
+              }}
+            >
+              {!teamOne.length ? <img src={plus} alt="Add" /> : "Edit"}
+            </button>
+          </div>
+          <div
+            className={`${styles.users__list} ${openTeamTwo ? `${styles.open}` : ""}`}
+          >
+            <h1>Team two</h1>
+            <span>{teamTwo.length} from 5</span>
+            <div className={styles.userScroll}>
+              {availableTeamTwo.map((user) => {
+                return (
+                  <div className={styles.wrapper} key={user.id}>
+                    <input
+                      className={styles.checkbox}
+                      type="checkbox"
+                      name="player"
+                      checked={teamTwo.some((player) => player.id === user.id)}
+                      onChange={(event) =>
+                        addPlayersToTeam(event, user, "teamTwo")
+                      }
+                      disabled={
+                        teamTwo.length === 5 &&
+                        !teamTwo.some((player) => player.id === user.id)
+                      }
+                    />
+                    <img
+                      className={styles.player__img}
+                      src={photo}
+                      alt={user.name}
+                    />
+                    <p className={styles.player__name}>
+                      {user.name} {user.surname}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+            <button
+              type="button"
+              className={styles.button}
+              onClick={(event) => {
+                toggleModal(event, "teamTwo");
+              }}
+            >
+              Close
+            </button>
+          </div>
+          <div className={styles.inputBlock}>
+            <label htmlFor="team-two">Team two</label>
+            <div
+              style={{
+                marginBottom: "10px",
+                display: "flex",
+                flexDirection: "row",
+                gap: "22px",
+                overflow: "scroll",
+              }}
+            >
+              {teamTwo.map((player) => {
+                return (
+                  <div key={player.id}>
+                    <img
+                      className={styles.player__img}
+                      src={photo}
+                      alt={player.name}
+                    />
+                    <p
+                      style={{
+                        fontSize: "18px",
+                        fontWeight: "700",
+                        margin: "0px",
+                        textAlign: "center",
+                      }}
+                    >
+                      {player.name.slice(0, 1)}.{player.surname.slice(0, 1)}.
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+            <button
+              className={styles.button}
+              onClick={(event) => {
+                toggleModal(event, "teamTwo");
+              }}
+            >
+              {!teamTwo.length ? <img src={plus} alt="Add" /> : "Edit"}
+            </button>
+          </div>
+          <input type="submit" value="Create" />
+        </div>
+      </form>
+    </>
+  );
+}
+
+export { MatchForm };
