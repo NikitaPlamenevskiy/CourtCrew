10a1fbbbad0b8a52effa3088bfdd3cfb2789b355
Nikita_Pl <bruuix@gmail.com>
2026-02-05 16:31:22 +0700

    status logic fix

--- src/components/matchCardLoader/MatchCardLoader.jsx
+++ src/components/matchCardLoader/MatchCardLoader.jsx
@@ -3,26 +3,22 @@ import styles from "./MatchCardLoader.module.css";
 function MatchCardLoader() {
   return (
     <div className={styles.card}>
+      <p className={styles.name}>Match</p>
       <div className={styles.wrapper}>
         <div className={styles.wrapper}>
           <h1 className={styles.status}></h1>
-          <div className={styles.status_color}></div>
         </div>
         <h2 className={styles.date}></h2>
         <div className={styles.wrapper}>
           <div className={styles.wrapper}>
-            <img className={styles.team__img} />
+            <div className={styles.team__img}></div>
             <h3 className={styles.team__name}></h3>
           </div>
-          <p className={styles.team__points}>0</p>
           <span className={styles.line}></span>
           <div className={styles.wrapper}>
-            <img
-              className={styles.team__img}
-            />
+            <div className={styles.team__img}></div>
             <h3 className={styles.team__name}></h3>
           </div>
-          <p className={styles.team__points}>0</p>
         </div>
       </div>
     </div>
